6880d4e0442e538910a9e73c3ee85ec094fc8f32
xXQuasarXx <stelarfield@protonmail.com>
2026-08-20 16:59:10 -0300

    HarmonyEngine expansion

--- harmony.hpp
+++ harmony.hpp
@@ -19,10 +19,55 @@ class HarmonyEngine{
         std::atomic<bool> isDecoderInitiated;
         std::atomic<bool> isDeviceInitiated;
 
+        static void dataCallback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount){
+
+            HarmonyEngine* pHarmony = (HarmonyEngine*)pDevice->pUserData;
+            if(pHarmony == NULL) return;
+
+            ma_uint64 framesRead = 0;
+            ma_data_source_read_pcm_frames(&pHarmony->decoder, pOutput, frameCount, &framesRead);
+
+            if(framesRead < frameCount){
+                pHarmony->isMusicFinished = true;
+            }
+
+            (void)pInput;
+        };
+
+
+
         // Unique properties
         std::queue<std::string> musicQueue;
 
         std::atomic<bool> isMusicFinished;
         std::atomic<bool> isMusicPaused;
     public:
+        HarmonyEngine(){
+            _decoderConfig = ma_decoder_config_init(ma_format_f32, 2, 44100);
+
+            _deviceConfig = ma_device_config_init(ma_device_type_playback);
+            _deviceConfig.playback.format = ma_format_f32;
+            _deviceConfig.playback.channels = 2;
+            _deviceConfig.sampleRate = 44100;
+            _deviceConfig.dataCallback = this->dataCallback;
+            _deviceConfig.pUserData = this;
+
+            isDecoderInitiated = false;
+            isDeviceInitiated = false;
+
+            isMusicFinished = false;
+            isMusicPaused = false;
+        };
+        ~HarmonyEngine(){
+            if(!isDeviceInitiated){
+                ma_device_uninit(&device);
+                isDeviceInitiated = false;
+            }
+
+            if(!isDecoderInitiated){
+                ma_decoder_uninit(&decoder);
+                isDecoderInitiated = false;
+            }
+        };
+        
 };
