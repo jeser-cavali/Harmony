d3194899ea7b75a468688f8e9765f0c94a49b891
xXQuasarXx <stelarfield@protonmail.com>
2026-08-21 11:45:06 -0300

    Creation of play() method

--- harmony.hpp
+++ harmony.hpp
@@ -1,11 +1,13 @@
 #pragma once
-#define MINIAUDIO_IMPLEMENTATION
 
 #include "miniaudio.h"
 
+#include <iostream>
 #include <atomic>
 #include <queue>
 #include <string>
+#include <filesystem>
+#include <thread>
 
 class HarmonyEngine{
     private:
@@ -34,11 +36,87 @@ class HarmonyEngine{
             (void)pInput;
         };
 
+        bool isValidFiletype(std::string filepath){
+            return (
+                filepath.contains(".mp3") ||
+                filepath.contains(".wav") ||
+                filepath.contains(".flac") ||
+                filepath.contains(".ogg")
+            );
+        };
 
+        bool initDecoder(std::string filepath){
+            if(ma_decoder_init_file(filepath.c_str(), &_decoderConfig, &decoder) != MA_SUCCESS){
+                return false;
+            }
+            isDecoderInitiated = true;
+            isMusicFinished = false;
+            return true;
+        };
+        void uninitDecoder(){
+            if(isDecoderInitiated){
+                ma_decoder_uninit(&decoder);
+                isDecoderInitiated = false;
+            }
+        };
+
+        bool initDevice(){
+            if(isDecoderInitiated){
+                if(ma_device_init(NULL, &_deviceConfig, &device) != MA_SUCCESS){
+                    uninitDecoder();
+                    return false;
+                }
+                isDeviceInitiated = true;
+                return true;
+            }
+            return false;
+        };
+
+        void uninitDevice(){
+            if(isDeviceInitiated){
+                ma_device_uninit(&device);
+                isDeviceInitiated = false;
+            }
+        };
+
+        void pauseDevice(){
+            isMusicPlaying = false;
+            ma_device_stop(&device);
+        }
+
+        void startDevice(){
+            if(isDeviceInitiated && isDecoderInitiated){
+                if(ma_device_start(&device) != MA_SUCCESS){
+                    uninitDevice();
+                    uninitDecoder();
+                    return;
+                }
+
+                isMusicPlaying = true;
+
+                while(!isMusicFinished){
+                    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                }
+
+                musicQueue.pop();
+                
+                if(!musicQueue.empty()){
+                    pauseDevice();
+                    uninitDecoder();
+                } else{
+                    uninitDevice();
+                    uninitDecoder();
+                }
+            }
+        }
 
         // Unique properties
         std::queue<std::string> musicQueue;
 
+        std::atomic<bool> loop;
+        std::atomic<bool> random;
+
+        std::atomic<bool> isMusicPlaying;
         std::atomic<bool> isMusicFinished;
         std::atomic<bool> isMusicPaused;
     public:
@@ -55,19 +133,66 @@ class HarmonyEngine{
             isDecoderInitiated = false;
             isDeviceInitiated = false;
 
+            loop = false;
+            random = false;
+
+            isMusicPlaying = false;
             isMusicFinished = false;
             isMusicPaused = false;
         };
         ~HarmonyEngine(){
-            if(!isDeviceInitiated){
+            if(isMusicPlaying){
+                ma_device_stop(&device);
+                isMusicPlaying = false;
+
                 ma_device_uninit(&device);
                 isDeviceInitiated = false;
-            }
 
-            if(!isDecoderInitiated){
                 ma_decoder_uninit(&decoder);
                 isDecoderInitiated = false;
+            } else{
+                if(isDeviceInitiated){
+                    ma_device_uninit(&device);
+                    isDeviceInitiated = false;
+                }
+
+                if(isDecoderInitiated && !isDeviceInitiated){
+                    ma_decoder_uninit(&decoder);
+                    isDecoderInitiated = false;
+                }
             }
         };
+        void play(std::string url){
+            // TODO: add custom error behaviour
+            // Adding music to queue
+            if(std::filesystem::exists(url)){
+                if(std::filesystem::is_regular_file(url) && isValidFiletype(url)){
+                    musicQueue.emplace(url);
+                } else if(std::filesystem::is_directory(url)){
+                    for(const auto& file : std::filesystem::directory_iterator(url)){
+                        if(isValidFiletype(file.path().string())){
+                            musicQueue.emplace(file.path().string());
+                        }
+                    }
+                }
+            }
+
+            if(!isMusicPlaying && !musicQueue.empty()){
+                // Initiate decoder
+                while(!musicQueue.empty()){
+                    if(!isDecoderInitiated){
+                        initDecoder(musicQueue.front());
+                    }
+
+                    if(!isDeviceInitiated){
+                        initDevice();
+                    }
+
+                    if(isDecoderInitiated && isDeviceInitiated){
+                        startDevice();
+                    }
+                }
+            }
+        }
         
 };
